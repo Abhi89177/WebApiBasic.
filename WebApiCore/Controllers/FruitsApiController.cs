f3e862e531715f2f533f71c8ba5374862a2b52cd
ab639 <Abhishek Yadav>
2024-09-10 23:19:13 +0530

    Add project files.

---
+++ WebApiCore/Controllers/FruitsApiController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitsApiController : ControllerBase
+    {
+        public List<string> fruits = new List<string>()
+        {
+            "Apple",
+            "Banana",
+            "Mango",
+            "Cherry",
+            "Graps",
+            "Guwava",
+            "Orange", 
+            "DryFruits",
+        };
+      
+        [HttpGet]
+        public List<string> GetFruits()
+        {
+            return fruits;
+        }
+
+        [HttpGet("{id}")]
+        public string GetFruits(int id)
+        {
+            return fruits.ElementAt(id);
+        }
+    }
+}
